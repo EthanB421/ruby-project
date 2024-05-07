345fc5b52460d6dcfc5c573ed2f9a7919b0d40d1
Ethan Bautista <89360058+EthanB421@users.noreply.github.com>
2024-05-06 21:59:48 -0700

    added quantity

--- scraper.rb
+++ scraper.rb
@@ -9,16 +9,28 @@ def scraper(page_number)
 
   products = []  # Stores product info
 
-  items = parsed_page.css('main ul.products li.product')  # Gets each item on the page
+  items = parsed_page.css('main ul.products li.product')  #Gets each item
 
   if items.empty?
     puts "No items found on page #{page_number}"
   else
     items.each do |item|
-      name = item.css('h2').text.strip  # Extract product name
-      price = item.css('span.price').text.strip  # Extract product price
-      product = { name: name, price: price }  # Create a hash representing the product
-      products << product  # Add the product to array
+      name = item.css('h2').text.strip  #Extract name
+      price = item.css('span.price').text.strip  #Extract price
+      
+      #Second url to add quantity since it was on another page
+      second_url = "https://scrapeme.live/shop/#{name}"
+      unparsed_second_page = HTTParty.get(second_url)
+      parsed_second_page = Nokogiri::HTML(unparsed_second_page)
+      
+      # Extract additional information from the second page
+      quantity_string = parsed_second_page.css('div.summary p.stock').text.strip  # Extractquantity as string
+
+      #Convert to integer
+      quantity = quantity_string.scan(/\d+/).first.to_i 
+
+      product = { name: name, price: price, quantity: quantity }  #Create a hash representing the product
+      products << product  # Add product to array
     end
   end
 
@@ -27,7 +39,7 @@ end
 
 # Scrape multiple pages and collect the data
 all_products = []
-(1..40).each do |page_number|
+(1..2).each do |page_number|
   all_products.concat(scraper(page_number))
 end
 
